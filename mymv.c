e3a3bf9739ee6e82b1953a38b1e50a1675232f78
KetanHukare <hukarekg15.comp@coep.ac.in>
2016-10-29 19:15:18 +0000

    Update mymv.c

--- mymv.c
+++ mymv.c
@@ -25,10 +25,7 @@
 void mymv(char *s, char *d) {
 	int fd1, fd2, count, buf[NUM];
 	fd1 = open(s, O_RDONLY);
-	
 	fd2 = open(d, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXG | S_IRWXU | S_IRWXO);
-	
-	
 	while((count = read(fd1, buf, NUM)) > 0) {
 		write(fd2, buf, count);
 		close(fd1);
